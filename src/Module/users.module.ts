d2c69ab18539bdbee77499b36a13b55d89ff4494
ainf024 <marcin.pigon@ab.pl>
2023-01-06 18:14:42 +0100

    add user entities to user module

--- src/Module/users.module.ts
+++ src/Module/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from '../Service/users.service';
 import { UsersController } from '../Controller/users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UzytkownicyEntity } from '../Entity/Uzytkownicy.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UzytkownicyEntity])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
